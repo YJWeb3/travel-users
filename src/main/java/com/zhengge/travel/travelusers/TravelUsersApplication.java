d2c336bb3781fa81ad508cced556613ea755dc9c
niezheng <niezheng@mokahr.com>
2024-07-10 22:21:06 +0800

    First construction

---
+++ src/main/java/com/zhengge/travel/travelusers/TravelUsersApplication.java
@@ -0,0 +1,13 @@
+package com.zhengge.travel.travelusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhengge/travel/travelusers/TravelUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhengge.travel.travelusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
